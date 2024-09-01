d6bde3d74b90126c16bc2008eaf0b45eca6d6ea6
GYoung <ytok1108@aol.com>
2024-09-01 21:12:20 +0900

    lvim_LinuxOS_final/

--- lua/user/whichkey.lua
+++
@@ -1,5 +0,0 @@
-local existingLspWhichKeys = lvim.builtin.which_key.mappings["l"]
-table.insert(existingLspWhichKeys, {
-  name = "Inlay Hints",
-  h = { "<cmd>lua require('lsp-inlayhints').toggle()<cr>", "Toggle Hints" },
-})
